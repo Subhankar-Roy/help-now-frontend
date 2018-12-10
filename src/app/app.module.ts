d198697308e57594a8603f3eaa350955ee091bdf
subhankar <subhankar.roy@techprostudio.com>
2018-12-10 19:16:58 +0530

    frontend landing page v0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingpageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
